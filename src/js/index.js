7057003bd050fe0cdf9f6264ff84e228074f6c0c
Gabriel DM <gnizto@outlook.com>
2023-02-21 00:58:32 -0300

    feat: added playRound and return result of match
    Next step: keep on step 5 and play more rounds

--- src/js/index.js
+++ src/js/index.js
@@ -1,8 +1,40 @@
+String.prototype.toTitleCase = function() {
+  let strLower = this.toLocaleLowerCase();
+  return strLower.slice(0,1).toUpperCase() + strLower.slice(1);
+}
+
 const getRandomNumber = (lessThan) => Math.floor(Math.random() * lessThan);
+const getBeatsSentence = (winner, looser) => `${winner.toTitleCase()} beats ${looser.toLowerCase()}!`;
+
+const choices = {
+  rock:     'scissors', 
+  paper:    'rock', 
+  scissors: 'paper'
+}
+
+const choicesArray = [...Object.keys(choices)];
 
 function getComputerChoice() {
-  // TODO: create logic here (step 3)
-  const choices = ['rock', 'paper', 'scissors'];
-  let choice = choices[getRandomNumber(choices.length)];
+  let choice = choicesArray[getRandomNumber(choicesArray.length)];
   return choice;
 }
+
+function playRound(playerSelection, computerSelection) {
+  let playerSelectionLower = playerSelection.toLowerCase();
+
+  let result = (choices[playerSelectionLower] === computerSelection) ?
+                     1 : (choices[computerSelection] === playerSelectionLower) ?
+                    -1 : 0;
+  let sentence = (result === 1) ?
+                    `You Win! ${getBeatsSentence(playerSelection, computerSelection)}`:
+                 (result === -1)?
+                    `You Loose! ${getBeatsSentence(computerSelection, playerSelection)}`:
+                    `It's a Draw! Both choose ${computerSelection}!`
+  console.log(sentence)
+}
+
+// TODO: keep on step 5
+let playerSelection = 'rock';
+let computerSelection = getComputerChoice();
+
+playRound(playerSelection, computerSelection);
