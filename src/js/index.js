a73f4b93bab08e48c84d42b784d91825f5609429
Gabriel DM <gnizto@outlook.com>
2023-02-20 23:44:50 -0300

    feat: added getComputerChoice to get random choice

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,8 @@
+const getRandomNumber = (lessThan) => Math.floor(Math.random() * lessThan);
+
 function getComputerChoice() {
   // TODO: create logic here (step 3)
+  const choices = ['rock', 'paper', 'scissors'];
+  let choice = choices[getRandomNumber(choices.length)];
+  return choice;
 }
